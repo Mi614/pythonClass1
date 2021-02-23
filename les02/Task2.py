9eca48435aef844be393cdc537704e37feaeb905
Mi614 <koyron@yandex.ru>
2021-02-23 12:10:37 +0300

    Les02

--- les02/Task2.py
+++ les02/Task2.py
@@ -1,14 +1,22 @@
 mylist = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 
+for i, el in enumerate(mylist):
+    if el.isdigit():
+        el = el.zfill(2)
+    # if el[0] == '-' or el[0] == '+':
+    if el.startswith('+') or el.startswith('-'):
+        el = el.zfill(3)
+    mylist[i] = el
+print(mylist)
 
 for el in mylist.copy():
     y = 0
     for d in el:
         if d.isdigit():
-            y += 1
-    if y > 0:
+            y = 1
+    if y == 1:
         mylist.append('"')
-        mylist.append(el.zfill(2))
+        mylist.append(el)
         mylist.append('"')
     else:
         mylist.append(el)
@@ -16,4 +24,10 @@ for el in mylist.copy():
 
 print(mylist)
 
-print(' '.join(mylist))
+my_str = ' '.join(mylist)
+
+for i in my_str.split():
+    if i.isdigit() or i.startswith('+') or i.startswith('-'):
+        my_str = my_str.replace(f' {i} ', i)
+
+print(my_str)
